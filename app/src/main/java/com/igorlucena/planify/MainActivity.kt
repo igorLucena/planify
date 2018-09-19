aaa0f1d220a8ae81e2393726a443731c6259ebdb
Igor Lucena <igorvlnascimento@gmail.com>
2018-09-18 22:53:17 -0300

    Async done

--- app/src/main/java/com/igorlucena/planify/MainActivity.kt
+++ app/src/main/java/com/igorlucena/planify/MainActivity.kt
@@ -4,9 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.ProgressBar
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+
+    var API_URL = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&titles="
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +31,19 @@ class MainActivity : AppCompatActivity() {
         photo_button.setOnClickListener {
             val intent = Intent("android.media.action.IMAGE_CAPTURE")
             startActivity(intent)
+        }
+
+        catchWikipedia()
+    }
+
+    private fun catchWikipedia() {
+        doAsync {
+            API_URL += "Boeing%20777"
+            val response = URL(API_URL).readText()
+
+            uiThread {
+                alert(response).show()
+            }
         }
     }
 }
